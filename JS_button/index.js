2407a97ab4e6cac1b40530b3a480475586fe80d9
abicarvajal <cheshire18eros@gmail.com>
2022-06-06 00:18:23 -0500

    Update index.js
    Add "blur" event listener that checks if it's a number a add the number of elements to the HTML

--- JS_button/index.js
+++ JS_button/index.js
@@ -1,33 +1,40 @@
 
 window.addEventListener('DOMContentLoaded', function(e){
     var maxnumber = document.getElementById("maxdiv");
+    var quantity = 0;
+    maxnumber.addEventListener("blur",(e)=>{
+        // console.log(maxnumber.value);
+        quantity = maxnumber.value;
+        if(!isNaN(quantity)){
+            var boton = document.getElementById("boton");
+            var contentcontainer = document.getElementsByClassName("content-container")[0];
+            for(let i = 0; i < quantity; i++){
+                var element =  document.createElement('div');
+                element.className = "content-container";
+                // element.innerHTML = contentcontainer.innerHTML;
+                element.innerHTML = `<div class="center-content"><input id="input_${i}" type="text" class="input" placeholder="Un campo de texto"><button class="button" id="boton_${i}">Boton</button></div>`
+                // element.getElementById('input').id = i;
+                document.body.appendChild(element);
+            }
 
-    // maxnumber.addEventListener("onchange",(e)=>{
+            for(let i = 0; i < quantity; i++)
+            {
+                var nuevoBoton = document.getElementById(`boton_${i}`);
+                nuevoBoton.addEventListener("click",(e)=>{
+                    var inputValue = document.getElementById(`input_${i}`).value;
+                    alert(inputValue);
+                })
+            }
 
-    // })
-    var boton = document.getElementById("boton");
-    var contentcontainer = document.getElementsByClassName("content-container")[0];
-    for(let i = 0; i < 10; i++){
-        var element =  document.createElement('div');
-        element.className = "content-container";
-        // element.innerHTML = contentcontainer.innerHTML;
-        element.innerHTML = `<div class="center-content"><input id="input_${i}" type="text" class="input" placeholder="Un campo de texto"><button class="button" id="boton_${i}">Boton</button></div>`
-        // element.getElementById('input').id = i;
-        document.body.appendChild(element);
-    }
+            boton.addEventListener("click",(e)=>{
+                // e.preventDefault();
+                var inputValue = document.getElementById("input").value;
+                alert(inputValue);
+            })
+        }
+        else{
+            alert("Ingrese un número de elementos");
+        }
 
-    for(let i = 0; i < 10; i++)
-    {
-        var nuevoBoton = document.getElementById(`boton_${i}`);
-        nuevoBoton.addEventListener("click",(e)=>{
-            var inputValue = document.getElementById(`input_${i}`).value;
-            alert(inputValue);
-        })
-    }
-
-    boton.addEventListener("click",(e)=>{
-        // e.preventDefault();
-        var inputValue = document.getElementById("input").value;
-        alert(inputValue);
     })
 })
